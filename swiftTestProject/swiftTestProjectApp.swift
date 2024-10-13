62ba2f2c0d0234029ea133077481a8186581c04f
Gretzki <gretzki@adesso-mobile.de>
2024-10-13 21:17:52 +0200

    main: create sample project in xcode15.3

---
+++ swiftTestProject/swiftTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftTestProjectApp.swift
+//  swiftTestProject
+//
+//  Created by Gretzki, Christopher on 13.10.24.
+//
+
+import SwiftUI
+
+@main
+struct swiftTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
